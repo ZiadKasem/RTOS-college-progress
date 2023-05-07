2fca9ec8b6574041d69b1baf81939382ea7f625a
ziad ashraf <81923383+zakoshgo@users.noreply.github.com>
2023-05-07 16:01:11 +0300

    Update Timer0.c

--- Project/code/Timer0.c
+++ Project/code/Timer0.c
@@ -1,22 +1,14 @@
-#include "Timer0.h"
-
-
+#include "tm4c123gh6pm.h"
 
 void TimerInit(){
-    //set the clock
-    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
-                   SYSCTL_XTAL_16MHZ);
-    //
-    // The Timer0 peripheral must be enabled for use.
-    //
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
-    //while(!SysCtlPeripheralReady(TIMER0_BASE));
-     //
-    // Configure Timer0 as a 32-bit periodic timer.
-    //
-    TimerConfigure(TIMER0_BASE,TIMER_CFG_A_PERIODIC);
-    //Set load value to done  250 ms
-    TimerLoadSet(TIMER0_BASE, TIMER_A, SysCtlClockGet());
-
+	
+	  SYSCTL_RCGCTIMER_R |= SYSCTL_RCGCTIMER_R0; // Enable Timer 0 clock
+		TIMER0_CTL_R &= !TIMER_CTL_TAEN; // Disable Timer 0
+		TIMER0_CFG_R = TIMER_CFG_32_BIT_TIMER; // Set Timer 0 as a 32-bit timer
+		TIMER0_TAMR_R = TIMER_TAMR_TAMR_PERIOD; // Set Timer 0 in periodic mode
+		TIMER0_TAILR_R = 7999; // Set the desired period value
+		TIMER0_IMR_R |= TIMER_IMR_TATOIM; // Enable Timer 0 timeout interrupt
+		NVIC_EN0_R |= 1 << (INT_TIMER0A - 16); // Enable Timer 0 interrupt in the NVIC
+		TIMER0_CTL_R |= TIMER_CTL_TAEN; // Enable Timer 0
 }
 // 5.2.5.2 clock config
